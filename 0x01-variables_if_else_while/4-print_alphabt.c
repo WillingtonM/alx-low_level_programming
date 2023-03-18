72945a4a71b93b34dae94a2dddce6c575c44a563
WillingtonM <willington.mhlanga@gmail.com>
2023-03-18 07:16:49 +0200

    Update 4-print_alphabt.c file

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,15 +1,10 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
-
-
 
 /**
  * main - Prints the alphabet in lowercase, except for q and e.
- 
+ *
  * Return: Always 0.
-*/
-
+ */
 int main(void)
 {
 	char letter;
